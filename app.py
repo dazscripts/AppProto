a6c61ae2caacb8e935c7ccf5676ecaa54992e784
daz <71845855+dazscripts@users.noreply.github.com>
2023-11-10 11:25:54 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,59 +1,55 @@
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, redirect, url_for, request, session, flash
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # Change this to a random secret key
 
-# Replace the user, password, database, and connection name with your actual Google Cloud SQL information
-# Format for PostgreSQL: 'postgresql://user:password@/database?host=/cloudsql/connection_name'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://<USER>:<PASSWORD>@/<DATABASE>?host=/cloudsql/<CONNECTION_NAME>'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+# Simulated data storage
+announcements = [
+    {"id": 1, "content": "Company picnic this Saturday!"},
+    {"id": 2, "content": "Reminder: Q3 reports are due next week."},
+]
 
-# Define models
-class Customer(db.Model):
-    __tablename__ = 'customer'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+# Simulated user accounts
+users = {
+    "employee": {"username": "employee", "password": "employee123", "role": "employee"},
+    "customer": {"username": "customer", "password": "customer123", "role": "customer"},
+}
 
-class Announcement(db.Model):
-    __tablename__ = 'announcement'
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(250), nullable=False)
-    posted_at = db.Column(db.DateTime, server_default=db.func.now())
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-# Create the tables
-with app.app_context():
-    db.create_all()
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = users.get(username)
+        if user and user['password'] == password:
+            session['username'] = user['username']
+            session['role'] = user['role']
+            return redirect(url_for('home'))
+        else:
+            flash('Invalid username or password')
+    return render_template('login.html')
 
-# Routes
-@app.route('/customers', methods=['POST'])
-def add_customer():
-    data = request.json
-    new_customer = Customer(name=data['name'], email=data['email'])
-    db.session.add(new_customer)
-    db.session.commit()
-    return jsonify({"message": "Customer added successfully"}), 201
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    session.pop('role', None)
+    return redirect(url_for('home'))
 
-@app.route('/customers', methods=['GET'])
-def get_customers():
-    customers_list = Customer.query.all()
-    customers = [{"id": customer.id, "name": customer.name, "email": customer.email} for customer in customers_list]
-    return jsonify(customers)
+@app.route('/announcements')
+def view_announcements():
+    return render_template('announcements.html', announcements=announcements)
 
-@app.route('/announcements', methods=['POST'])
-def post_announcement():
-    data = request.json
-    new_announcement = Announcement(content=data['content'])
-    db.session.add(new_announcement)
-    db.session.commit()
-    return jsonify({"message": "Announcement posted successfully"}), 201
+@app.route('/calendar')
+def view_calendar():
+    return render_template('calendar.html')
 
-@app.route('/announcements', methods=['GET'])
-def get_announcements():
-    announcements_list = Announcement.query.all()
-    announcements = [{"id": announcement.id, "content": announcement.content, "posted_at": announcement.posted_at} for announcement in announcements_list]
-    return jsonify(announcements)
+@app.route('/references')
+def view_references():
+    return render_template('references.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
